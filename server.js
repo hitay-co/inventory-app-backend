ceb10c75b62a1dce02fcef58f309d990f801ad98
hitay-co <107373502+hitay-co@users.noreply.github.com>
2022-07-06 18:47:04 +0300

    Basic server Express setup and env file created.

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Server....');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to Inventory App',
+  });
+});
