d2fd52b0c337f7fb9dd3f89a3d534d0784005049
hitay-co <107373502+hitay-co@users.noreply.github.com>
2022-07-07 23:27:59 +0300

    installed express-async-handler npm package. created connectDB function and connected the app with MongoDB.

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,15 +1,18 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc   Create an Inventory
 // @route  /api/inventories
 // @access Public
-const createInventory = (req, res) => {
+const createInventory = asyncHandler(async (req, res) => {
   const { name, type, quantity } = req.body;
 
   if (!name || !type || !quantity) {
+    res.status(400);
     throw new Error('Please include all fields');
   }
 
   res.send('Inventory Route');
-};
+});
 
 module.exports = {
   createInventory,
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 8000;
 const inventoryRoutes = require('./routes/inventoryRoutes');
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
