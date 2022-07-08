246382d5b4278d2176e8d9699480bd1c8a4e4055
hitay-co <107373502+hitay-co@users.noreply.github.com>
2022-07-08 16:19:11 +0300

    update and delete inventory routes created and implemented.

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,6 +1,15 @@
 const asyncHandler = require('express-async-handler');
 const Inventory = require('../models/inventoryModel');
 
+// @desc   Get Inventories
+// @route  GET /api/inventories
+// @access Public
+const getInventories = asyncHandler(async (req, res) => {
+  const inventories = await Inventory.find();
+
+  res.status(200).json(inventories);
+});
+
 // @desc   Create an Inventory
 // @route  /api/inventories
 // @access Public
@@ -28,16 +37,45 @@ const createInventory = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc   Get Inventories
-// @route  GET /api/inventories
+// @desc   Update Inventory
+// @route  PUT /api/inventories/:id
 // @access Public
-const getInventories = asyncHandler(async (req, res) => {
-  const inventories = await Inventory.find();
+const updateInventory = asyncHandler(async (req, res) => {
+  const inventory = await Inventory.findById(req.params.id);
 
-  res.status(200).json(inventories);
+  if (!inventory) {
+    res.status(400);
+    throw new Error('Inventory not found');
+  }
+
+  const updatedInventory = await Inventory.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedInventory);
+});
+
+// @desc   Delete Inventory
+// @route  DELETE /api/inventories/:id
+// @access Public
+const deleteInventory = asyncHandler(async (req, res) => {
+  const inventory = await Inventory.findById(req.params.id);
+
+  if (!inventory) {
+    res.status(400);
+    throw new Error('Inventory not found');
+  }
+
+  await inventory.remove();
+
+  res.status(200).json({ success: true });
 });
 
 module.exports = {
-  createInventory,
   getInventories,
+  createInventory,
+  updateInventory,
+  deleteInventory,
 };
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const {
   createInventory,
   getInventories,
+  updateInventory,
+  deleteInventory,
 } = require('../controllers/inventoryController');
 
 router.route('/').get(getInventories).post(createInventory);
+
+router.route('/:id').put(updateInventory).delete(deleteInventory);
 
 module.exports = router;
